b88b8eccfb09ca9ad2ad7fcd8a217f2bdf5dc169
Kevin Gregoire <kevgreg94@gmail.com>
2022-09-06 22:40:06 -0500

    Rock Paper Scissors complete

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function getComputerChoice() {
 //Function to prompt the player for their choice or rock, paper, or scissors
 function getPlayerChoice() {
     let input = prompt("Choose rock, paper, or scissors").toLowerCase();
-    //console.log(input);
+    //Validates user input and prompts again if not rock, paper, or scissors
     while (true) {
         if (input == "rock" || input == "paper" || input == "scissors") {
             return input;
@@ -29,24 +29,60 @@ function getPlayerChoice() {
     }
 }
 
+//Function to play one round and declare win, lose, or tie
 function playRound() {
     let computerSelection = getComputerChoice();
     let playerSelection = getPlayerChoice();
 
     //Tie conditions
     if ((playerSelection == "rock" && computerSelection == "rock") || (playerSelection == "paper" && computerSelection == "paper") || (playerSelection == "scissors" && computerSelection == "scissors")) {
-        console.log("Tie" + ". Player:" + playerSelection + ". Computer: " + computerSelection)
+        console.log("Tie" + ". Player:" + playerSelection + ". Computer: " + computerSelection);
+
+        //returns
+        return 1;
     }//Win conditions
     else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
-        console.log("You win" + ". Player:" + playerSelection + ". Computer: " + computerSelection)
+        console.log("You win" + ". Player:" + playerSelection + ". Computer: " + computerSelection);
+
+        return 2;
     }
-    else{//Lose conditions
-        console.log("You lose" + ". Player:" + playerSelection + ". Computer: " + computerSelection)
+    else {//Lose conditions
+        console.log("You lose" + ". Player:" + playerSelection + ". Computer: " + computerSelection);
+
+        return 3;
     }
 }
 
+//function to play 5 rounds and keep score
 function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+
+    for (let i = 0; i < 5;) {
+
+        let result = playRound();
+
+        if (result == 2) {
+            playerScore += 1;
+            i += 1;
+        }
+        else if (result == 3) {
+            computerScore += 1;
+            i += 1;
+        }
+        else { }
+    }
+
+    console.log("Player score: " + playerScore)
+    console.log("Computer score: " + computerScore)
+
+    if (playerScore > computerScore) {
+        console.log("You win!")
+    }
+    else (
+        console.log("You lose.")
+    )
 
 }
-playRound()
-//console.log("You chose " + getPlayerChoice());
+
+game();
